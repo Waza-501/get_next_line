44391782b9deaa212388bc6a6ba820be7c24bddc
Owen Hearn <ohearn@f1r6s18.codam.nl>
2022-09-07 15:17:05 +0200

    first day of project

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,29 @@
 /*   By: ohearn <ohearn@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/07 10:56:57 by ohearn        #+#    #+#                 */
-/*   Updated: 2022/09/07 13:34:48 by ohearn        ########   odam.nl         */
+/*   Updated: 2022/09/07 14:17:22 by ohearn        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 
 char	*get_next_line(int fd)
 {
-	static int	stash;
-	int			*ret;
-	
+	static char	stash = NULL;
+	char		buffer;
+	char		line;
+	char		*ret;
+
+	if (fd < 0 || fd > 1024)
+		return (NULL);
+	if (!stash)
+	{
+		stash = (char*)malloc(sizeof(char));
+		if (!stash)
+			return (NULL);
+		stash = "\0";
+	}
 }
